d92959a683d041b07593c55438bc2f83f1f76a58
ganeisha2009-ux <ganeisha2009@gmail.com>
2026-01-12 10:03:48 +0700

    Tambah input nilai & nama

--- NilaiSiswaApp/Program.cs
+++ NilaiSiswaApp/Program.cs
@@ -2,3 +2,11 @@
 string nama;
 
 Console.WriteLine("Aplikasi Nilai Siswa");
+
+Console.Write("Input Nama: ");
+nama = Console.ReadLine();
+
+Console.Write("Input Nilai: ");
+nilai = int.Parse(Console.ReadLine());
+
+ 
